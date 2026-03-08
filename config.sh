0ad02b51d43e653b7cd1e7ec2a32dbea1280fd82
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-08 14:55:49 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
-set -e
 
-TARGET="$1"
+cd openwrt
 
-echo "create WR720N v3 dts..."
+echo "Add WR720N DTS"
 
-cat > target/linux/ath79/dts/ar9331_tplink_tl-wr720n-v3.dts << 'EOF'
-/dts-v1/;
+cp ../dts/ar9331_tplink_tl-wr720n-v3.dts target/linux/ath79/dts/
 
-#include "ar9331_tplink_tl-wr720n.dtsi"
-
-/ {
-    compatible = "tplink,tl-wr720n-v3", "qca,ar9331";
-    model = "TP-Link TL-WR720N v3";
-};
-
-&switch0 {
-    ports {
-        port@0 { reg = <0>; label = "wan"; };
-        port@1 { reg = <1>; label = "lan1"; };
-        port@2 { reg = <2>; label = "lan2"; };
-        port@6 { reg = <6>; label = "cpu"; ethernet = <&eth0>; };
-    };
-};
-EOF
-
-echo "add device..."
+echo "Add device definition"
 
 cat >> target/linux/ath79/image/tiny-tp-link.mk << 'EOF'
 
@@ -35,26 +16,27 @@ define Device/tplink_tl-wr720n-v3
   DEVICE_MODEL := TL-WR720N
   DEVICE_VARIANT := v3
   IMAGE_SIZE := 15872k
-  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-usb-printer p910nd
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-printer p910nd
 endef
 TARGET_DEVICES += tplink_tl-wr720n-v3
 
 EOF
 
-echo "create .config ..."
+echo "Create config"
 
-cat > .config << 'EOF'
+cat >> .config << EOF
+
 CONFIG_TARGET_ath79=y
 CONFIG_TARGET_ath79_tiny=y
 CONFIG_TARGET_ath79_tiny_DEVICE_tplink_tl-wr720n-v3=y
 
 CONFIG_PACKAGE_luci=y
-CONFIG_PACKAGE_luci-i18n-base-zh-cn=y
-
-# CONFIG_PACKAGE_luci-app-ssr-plus=y  # 如果要 SSR+，取消注释即可
 
 CONFIG_PACKAGE_kmod-usb2=y
 CONFIG_PACKAGE_kmod-usb-ohci=y
 CONFIG_PACKAGE_kmod-usb-printer=y
 CONFIG_PACKAGE_p910nd=y
+
 EOF
+
+make defconfig
