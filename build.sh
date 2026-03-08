e3b2a0355659e1e2e7f44941cb573d52c759a43d
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-08 19:50:56 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,21 +3,24 @@ set -e
 
 VERSION=$1
 
+echo "Cloning OpenWrt $VERSION..."
+
 git clone https://github.com/openwrt/openwrt -b $VERSION openwrt
 
 cd openwrt
 
+echo "Update feeds..."
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
 cd ..
 
+echo "Generate config..."
 bash config.sh
 
 cd openwrt
 
-rm -f .config
-cp ../.config .
-
 make defconfig
+
+echo "Start build..."
 make -j$(nproc)
