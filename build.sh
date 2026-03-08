553986fb540fd0b263285951cb344dcdb60627f6
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-08 11:49:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 VERSION=$1
@@ -7,20 +6,24 @@ TARGET=$2
 
 echo "build target start ..."
 
+# 克隆 openwrt
 git clone https://github.com/openwrt/openwrt -b $VERSION openwrt
 
+# 进入 openwrt
 cd openwrt
 
+# 更新 feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
+# 先生成默认配置
 make defconfig
 
-cd ..
+# 运行 config.sh（注意：config.sh 不再 cd openwrt）
+bash ../config.sh $TARGET
 
-bash config.sh
-
-cd openwrt
-
+# 确保 .config 更新
 make defconfig
 
+# 编译
 make -j$(nproc)
