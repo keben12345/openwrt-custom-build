b9c3ddedd347581e5e8ec65e82267208c2402c1b
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-09 00:11:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 set -e
 
-VERSION=$1
+VERSION=$1   # 例: v22.03.5
+TARGET=$2    # 例: ath79/generic/tplink_tl-wr710n-usb
 
-echo "Clone OpenWrt..."
-
-git clone https://github.com/openwrt/openwrt -b $VERSION openwrt
+echo "Cloning OpenWrt $VERSION ..."
+git clone -b $VERSION https://github.com/openwrt/openwrt openwrt
 
 cd openwrt
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
+# 调用配置脚本
 cd ..
-
 bash config.sh
 
 cd openwrt
 
 make defconfig
-
-echo "Build firmware..."
-
 make -j$(nproc)
