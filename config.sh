7fcf00f018adb0d88bd19e183c043a5824193c87
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-08 16:59:56 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+set -e
 
 cd openwrt
 
-echo "Add WR720N DTS"
-
+echo "create WR720N v3 dts..."
+mkdir -p target/linux/ath79/dts
 cp ../dts/ar9331_tplink_tl-wr720n-v3.dts target/linux/ath79/dts/
 
-echo "Add device definition"
-
+echo "add device mk..."
 cat >> target/linux/ath79/image/tiny-tp-link.mk << 'EOF'
 
 define Device/tplink_tl-wr720n-v3
@@ -16,44 +16,31 @@ define Device/tplink_tl-wr720n-v3
   DEVICE_MODEL := TL-WR720N
   DEVICE_VARIANT := v3
   IMAGE_SIZE := 15872k
+  TPLINK_HWID := 0x07030101
   DEVICE_PACKAGES := \
-        kmod-usb2 \
-        kmod-usb-ohci \
-        kmod-usb-storage \
-        kmod-usb-printer \
-        kmod-fs-ext4 \
-        kmod-fs-vfat \
-        block-mount \
-        samba4-server \
-        p910nd
+    kmod-usb-core \
+    kmod-usb-uhci \
+    kmod-usb-ohci \
+    kmod-usb-printer \
+    p910nd \
+    luci \
+    luci-i18n-base-zh-cn
+  FIRMWARES := factory sysupgrade
 endef
 TARGET_DEVICES += tplink_tl-wr720n-v3
-
 EOF
 
-echo "Create config"
-
-cat >> .config << EOF
-
+echo "create .config..."
+cat > .config << 'EOF'
 CONFIG_TARGET_ath79=y
 CONFIG_TARGET_ath79_tiny=y
 CONFIG_TARGET_ath79_tiny_DEVICE_tplink_tl-wr720n-v3=y
 
 CONFIG_PACKAGE_luci=y
+CONFIG_PACKAGE_luci-i18n-base-zh-cn=y
 
 CONFIG_PACKAGE_kmod-usb2=y
 CONFIG_PACKAGE_kmod-usb-ohci=y
-CONFIG_PACKAGE_kmod-usb-storage=y
 CONFIG_PACKAGE_kmod-usb-printer=y
-
-CONFIG_PACKAGE_block-mount=y
-CONFIG_PACKAGE_kmod-fs-ext4=y
-CONFIG_PACKAGE_kmod-fs-vfat=y
-
-CONFIG_PACKAGE_samba4-server=y
-
 CONFIG_PACKAGE_p910nd=y
-
 EOF
-
-make defconfig
