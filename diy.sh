47375149c70b9d731521fcaa6116e703d2f3ef8f
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-09 14:14:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo "Apply WR720N 16MB flash patch..."
+echo "Patch WR720N flash..."
 
+# 16MB flash 分区
 sed -i 's/0x7d0000/0xfd0000/g' target/linux/ath79/dts/ar9331_tplink_tl-wr710n-8m.dtsi
 sed -i 's/0x7f0000/0xff0000/g' target/linux/ath79/dts/ar9331_tplink_tl-wr710n-8m.dtsi
 
-echo "Flash patch done."
+# 8MB layout → 16MB layout
+sed -i 's/tplink-8mlzma/tplink-16mlzma/g' target/linux/ath79/image/generic-tp-link.mk
+
+echo "Patch done."
