9b7b97e1be3edfec0b31712c006e0959aa0684e9
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-09 22:52:55 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -11,7 +11,13 @@ CONFIG_TARGET_ath79_generic_DEVICE_tplink_tl-wr710n-v1=y
 
 # LuCI
 CONFIG_PACKAGE_luci=y
+CONFIG_PACKAGE_luci-base=y
+CONFIG_LUCI_JSMIN=y
+CONFIG_LUCI_CSSTIDY=y
+CONFIG_LUCI_LANG_zh_Hans=y
 CONFIG_PACKAGE_luci-i18n-base-zh-cn=y
+CONFIG_PACKAGE_luci-compat=y
+CONFIG_PACKAGE_luci-mod-admin-full=y
 
 # USB 支持
 CONFIG_PACKAGE_kmod-usb2=y
@@ -25,10 +31,14 @@ CONFIG_PACKAGE_block-mount=y
 # USB 打印
 CONFIG_PACKAGE_kmod-usb-printer=y
 CONFIG_PACKAGE_p910nd=y
+CONFIG_PACKAGE_luci-app-p910nd=y
+CONFIG_PACKAGE_luci-i18n-p910nd-zh-cn=y
 
 # 常用工具
 CONFIG_PACKAGE_htop=y
 CONFIG_PACKAGE_nano=y
+CONFIG_PACKAGE_luci-app-ttyd=y
+CONFIG_PACKAGE_luci-i18n-ttyd-zh-cn=y
 
 EOF
 
