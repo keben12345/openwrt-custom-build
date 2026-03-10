8dd5bca1c049c31b423c8a61e94a4fa10aaa9ece
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-10 12:49:07 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -89,17 +89,14 @@ EOF
 
 echo "Patching image mk..."
 
-cat >> target/linux/ath79/image/generic-tp-link.mk << "EOF"
-
-define Device/tplink_tl-wr720n-v3
-  \$(Device/tplink-16mlzma)
-  SOC := ar9331
-  DEVICE_MODEL := TL-WR720N
-  DEVICE_VARIANT := v3
-  DEVICE_PACKAGES := kmod-usb-chipidea2 kmod-usb-ledtrig-usbport
-  TPLINK_HWID := 0x07200103
-  SUPPORTED_DEVICES += tl-wr720n
-endef
-TARGET_DEVICES += tplink_tl-wr720n-v3
-
-EOF
+sed -i '$a \
+define Device/tplink_tl-wr720n-v3\n\
+  $(Device/tplink-16mlzma)\n\
+  SOC := ar9331\n\
+  DEVICE_MODEL := TL-WR720N\n\
+  DEVICE_VARIANT := v3\n\
+  DEVICE_PACKAGES := kmod-usb-chipidea2 kmod-usb-ledtrig-usbport\n\
+  TPLINK_HWID := 0x07200103\n\
+endef\n\
+TARGET_DEVICES += tplink_tl-wr720n-v3\n' \
+target/linux/ath79/image/generic-tp-link.mk
