201369b523029aa3259e3edf00b3e6736a045a77
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-10 20:08:25 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -12,17 +12,17 @@ CONFIG_PACKAGE_luci=y
 CONFIG_PACKAGE_luci-base=y
 CONFIG_LUCI_JSMIN=y
 CONFIG_LUCI_CSSTIDY=y
-CONFIG_LUCI_LANG_zh_Hans=y
+# CONFIG_LUCI_LANG_zh_Hans=y
 CONFIG_PACKAGE_luci-i18n-base-zh-cn=y
 CONFIG_PACKAGE_luci-compat=y
 CONFIG_PACKAGE_luci-mod-admin-full=y
 # USB 支持
 CONFIG_PACKAGE_kmod-usb2=y
 CONFIG_PACKAGE_kmod-usb-ohci=y
-CONFIG_PACKAGE_kmod-usb-storage=y
+# CONFIG_PACKAGE_kmod-usb-storage=y
 # USB 文件系统
-CONFIG_PACKAGE_kmod-fs-ext4=y
-CONFIG_PACKAGE_block-mount=y
+# CONFIG_PACKAGE_kmod-fs-ext4=y
+# CONFIG_PACKAGE_block-mount=y
 # USB 打印
 CONFIG_PACKAGE_kmod-usb-printer=y
 CONFIG_PACKAGE_p910nd=y
