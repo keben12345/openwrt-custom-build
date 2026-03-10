2c10fda0e00a94f601485881069c4b869009dc1e
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-10 20:18:43 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -17,15 +17,15 @@ CONFIG_PACKAGE_luci-i18n-base-zh-cn=y
 CONFIG_PACKAGE_luci-compat=y
 CONFIG_PACKAGE_luci-mod-admin-full=y
 # USB 支持
-CONFIG_PACKAGE_kmod-usb2=y
-CONFIG_PACKAGE_kmod-usb-ohci=y
+# CONFIG_PACKAGE_kmod-usb2=y
+# CONFIG_PACKAGE_kmod-usb-ohci=y
 # CONFIG_PACKAGE_kmod-usb-storage=y
 # USB 文件系统
 # CONFIG_PACKAGE_kmod-fs-ext4=y
 # CONFIG_PACKAGE_block-mount=y
 # USB 打印
-CONFIG_PACKAGE_kmod-usb-printer=y
-CONFIG_PACKAGE_p910nd=y
+# CONFIG_PACKAGE_kmod-usb-printer=y
+# CONFIG_PACKAGE_p910nd=y
 CONFIG_PACKAGE_luci-app-p910nd=y
 CONFIG_PACKAGE_luci-i18n-p910nd-zh-cn=y
 CONFIG_PACKAGE_kmod-usb2=y
