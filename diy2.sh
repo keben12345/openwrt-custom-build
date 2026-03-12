42642a749c31ccfcbeaa5d4387c3a5c9c7049e39
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-13 00:31:39 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -165,8 +165,9 @@ define Device/tplink_tl-wr720n-v3
   TPLINK_HWID := 0x07200103
   DEVICE_PACKAGES := \
         kmod-usb-core \
-        kmod-usb-ohci 
-	    SUPPORTED_DEVICES += tl-wr720n-v3
+        kmod-usb2 \
+        kmod-usb-ledtrig-usbport
+  SUPPORTED_DEVICES += tl-wr720n-v3
 endef
 TARGET_DEVICES += tplink_tl-wr720n-v3
 EOF
