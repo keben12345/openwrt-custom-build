0e0f58743964e051bca43973f518efa21f49febd
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-16 07:17:05 +0800

    Update diy21.sh

--- diy21.sh
+++ diy21.sh
@@ -10,138 +10,135 @@ echo "Adding WR720N v3 support"
 
 cat > target/linux/ath79/dts/ar9331_tplink_tl-wr720n.dtsi << 'EOF'
 #include "ar9331.dtsi"
+
 #include <dt-bindings/gpio/gpio.h>
 #include <dt-bindings/input/input.h>
 
 / {
-    aliases {
-        led-boot = &led_system;
-        led-failsafe = &led_system;
-        led-running = &led_system;
-        led-upgrade = &led_system;
-        label-mac-device = &wmac;
-    };
+	aliases {
+		led-boot = &led_system;
+		led-failsafe = &led_system;
+		led-running = &led_system;
+		led-upgrade = &led_system;
+		label-mac-device = &eth0;
+	};
 
-    keys {
-        compatible = "gpio-keys";
+	keys {
+		compatible = "gpio-keys";
 
-        reset {
-            label = "reset";
-            linux,code = <KEY_RESTART>;
-            gpios = <&gpio 11 GPIO_ACTIVE_LOW>;
-            debounce-interval = <60>;
-        };
+		reset {
+			label = "reset";
+			linux,code = <KEY_RESTART>;
+			gpios = <&gpio 11 GPIO_ACTIVE_LOW>;
+			debounce-interval = <60>;
+		};
 
-        sw1 {
-            label = "sw1";
-            linux,input-type = <EV_SW>;
-            linux,code = <BTN_0>;
-            gpios = <&gpio 18 GPIO_ACTIVE_HIGH>;
-            debounce-interval = <60>;
-        };
+		sw1 {
+			label = "sw1";
+			linux,input-type = <EV_SW>;
+			linux,code = <BTN_0>;
+			gpios = <&gpio 18 GPIO_ACTIVE_HIGH>;
+			debounce-interval = <60>;
+		};
 
-        sw2 {
-            label = "sw2";
-            linux,input-type = <EV_SW>;
-            linux,code = <BTN_1>;
-            gpios = <&gpio 20 GPIO_ACTIVE_HIGH>;
-            debounce-interval = <60>;
-        };
-    };
+		sw2 {
+			label = "sw2";
+			linux,input-type = <EV_SW>;
+			linux,code = <BTN_1>;
+			gpios = <&gpio 20 GPIO_ACTIVE_HIGH>;
+			debounce-interval = <60>;
+		};
+	};
 
-    leds {
-        compatible = "gpio-leds";
-        led_system: system {
-            label = "blue:system";
-            gpios = <&gpio 27 GPIO_ACTIVE_LOW>;
-        };
-    };
+	leds {
+		compatible = "gpio-leds";
 
-    reg_usb_vbus: regulator {
-        compatible = "regulator-fixed";
-        regulator-name = "usb_vbus";
-        regulator-min-microvolt = <5000000>;
-        regulator-max-microvolt = <5000000>;
-        gpio = <&gpio 8 GPIO_ACTIVE_HIGH>;
-        enable-active-high;
-    };
+		led_system: system {
+			label = "blue:system";
+			gpios = <&gpio 27 GPIO_ACTIVE_LOW>;
+		};
+	};
+
+	reg_usb_vbus: regulator {
+		compatible = "regulator-fixed";
+		regulator-name = "usb_vbus";
+		regulator-min-microvolt = <5000000>;
+		regulator-max-microvolt = <5000000>;
+		gpio = <&gpio 8 GPIO_ACTIVE_HIGH>;
+		enable-active-high;
+	};
 };
 
-/* WAN 接口 (eth0) */
 &eth0 {
-    status = "okay";
+	status = "okay";
 
-    nvmem-cells = <&macaddr_uboot>;
-    nvmem-cell-names = "mac-address";
-    mac-address-increment = <0>; /* WAN 不偏移 */
+	mtd-mac-address = <&uboot 0x124e0>;
 
-    gmac-config {
-        device = <&gmac>;
-        switch-phy-swap = <0>;
-        switch-phy-addr-swap = <0>;
-    };
+	gmac-config {
+		device = <&gmac>;
+
+		switch-phy-addr-swap = <0>;
+		switch-phy-swap = <0>;
+	};
 };
 
-/* LAN 接口 (eth1) */
 &eth1 {
-    status = "okay";
+	status = "okay";
 
-    nvmem-cells = <&macaddr_uboot>;
-    nvmem-cell-names = "mac-address";
-    mac-address-increment = <1>; /* LAN 偏移 +1 */
+	mtd-mac-address = <&uboot 0x124e0>;
+	mtd-mac-address-increment = <(-1)>;
 };
 
-/* USB */
 &usb {
-    status = "okay";
-    dr_mode = "host";
-    vbus-supply = <&reg_usb_vbus>;
+	status = "okay";
+
+	dr_mode = "host";
+	vbus-supply = <&reg_usb_vbus>;
 };
 
 &usb_phy {
-    status = "okay";
+	status = "okay";
 };
 
-/* WiFi */
 &wmac {
-    status = "okay";
-    mtd-cal-data = <&art 0x1000>;
-    nvmem-cells = <&macaddr_uboot>;
-    nvmem-cell-names = "mac-address";
-    mac-address-increment = <2>; /* WiFi 偏移 +2 */
+	status = "okay";
+
+	mtd-cal-data = <&art 0x1000>;
+	mtd-mac-address = <&uboot 0x124e0>;
 };
 
-/* SPI FLASH */
 &spi {
-    status = "okay";
+	status = "okay";
 
-    flash@0 {
-        compatible = "jedec,spi-nor";
-        reg = <0>;
-        spi-max-frequency = <25000000>;
+	flash@0 {
+		compatible = "jedec,spi-nor";
+		reg = <0>;
+		spi-max-frequency = <25000000>;
 
-        partitions {
-            compatible = "fixed-partitions";
+		partitions {
+			compatible = "fixed-partitions";
+			#address-cells = <1>;
+			#size-cells = <1>;
 
-            uboot: partition@0 {
-                reg = <0x0 0x020000>;
-                label = "u-boot";
-                read-only;
-            };
+			uboot: partition@0 {
+				reg = <0x0 0x20000>;
+				label = "u-boot";
+				read-only;
+			};
 
-            partition@20000 {
-                compatible = "tplink,firmware";
-                reg = <0x020000 0xfb0000>;
-                label = "firmware";
-            };
+			partition@20000 {
+				compatible = "tplink,firmware";
+				reg = <0x20000 0xfd0000>;
+				label = "firmware";
+			};
 
-            art: partition@ff0000 {
-                reg = <0xff0000 0x10000>;
-                label = "art";
-                read-only;
-            };
-        };
-    };
+			art: partition@7f0000 {
+				reg = <0xff0000 0x10000>;
+				label = "art";
+				read-only;
+			};
+		};
+	};
 };
 
 /* U-Boot MAC 地址 */
@@ -170,17 +167,15 @@ EOF
 # Device profile
 ############################
 
-cat > target/linux/ath79/image/generic-tp-link.mk << 'EOF'
+cat >> target/linux/ath79/image/generic-tp-link.mk << 'EOF'
 define Device/tplink_tl-wr720n-v3
   $(Device/tplink-8mlzma)
   SOC := ar9331
   DEVICE_MODEL := TL-WR720N
   DEVICE_VARIANT := v3
   TPLINK_HWID := 0x07200103
-  DEVICE_PACKAGES := \
-        kmod-usb-core \
-        kmod-usb2
-	    SUPPORTED_DEVICES += tl-wr720n-v3
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2
+  SUPPORTED_DEVICES += tl-wr720n-v3
 endef
 TARGET_DEVICES += tplink_tl-wr720n-v3
 EOF
@@ -189,10 +184,5 @@ EOF
 # build config
 ############################
 
-cat > .config << 'EOF'
-CONFIG_TARGET_ath79=y
-CONFIG_TARGET_ath79_generic=y
-CONFIG_TARGET_ath79_generic_DEVICE_tplink_tl-wr720n-v3=y
-EOF
 
 echo "WR720N patch applied"
