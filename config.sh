782a052e0b4681f2443ea42094800eb241a9d965
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-18 23:22:31 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -9,15 +9,14 @@ CONFIG_TARGET_ath79_generic=y
 CONFIG_TARGET_ath79_generic_DEVICE_tplink_tl-wr720n-v3=y
 # LuCI
 CONFIG_PACKAGE_luci=y
-CONFIG_PACKAGE_luci-base=y
 CONFIG_PACKAGE_luci-mod-admin-full=y
 CONFIG_LUCI_LANG_zh_Hans=y
 CONFIG_PACKAGE_luci-theme-bootstrap=y
 CONFIG_PACKAGE_luci-i18n-base-zh-cn=y
-CONFIG_PACKAGE_uhttpd=y
-CONFIG_PACKAGE_rpcd=y
-CONFIG_PACKAGE_rpcd-mod-luci=y
-CONFIG_PACKAGE_rpcd-mod-rrdns=y
+#CONFIG_PACKAGE_uhttpd=y
+#CONFIG_PACKAGE_rpcd=y
+#CONFIG_PACKAGE_rpcd-mod-luci=y
+#CONFIG_PACKAGE_rpcd-mod-rrdns=y
 CONFIG_PACKAGE_luci-app-opkg=y
 # USB 支持
 # CONFIG_PACKAGE_kmod-usb2=y
@@ -27,19 +26,19 @@ CONFIG_PACKAGE_luci-app-opkg=y
 # CONFIG_PACKAGE_kmod-fs-ext4=y
 # CONFIG_PACKAGE_block-mount=y
 # USB 打印
-CONFIG_PACKAGE_kmod-usb-printer=y
-CONFIG_PACKAGE_p910nd=y
-CONFIG_PACKAGE_luci-app-p910nd=y
-CONFIG_PACKAGE_luci-i18n-p910nd-zh-cn=y
-CONFIG_PACKAGE_usbutils=y
+#CONFIG_PACKAGE_kmod-usb-printer=y
+#CONFIG_PACKAGE_p910nd=y
+#CONFIG_PACKAGE_luci-app-p910nd=y
+#CONFIG_PACKAGE_luci-i18n-p910nd-zh-cn=y
+#CONFIG_PACKAGE_usbutils=y
 # 常用工具
-CONFIG_PACKAGE_htop=y
-CONFIG_PACKAGE_nano=y
+#CONFIG_PACKAGE_htop=y
+#CONFIG_PACKAGE_nano=y
 CONFIG_PACKAGE_luci-app-opkg=y
-CONFIG_PACKAGE_luci-app-watchcat=y
-CONFIG_PACKAGE_luci-app-ttyd=y
-CONFIG_PACKAGE_luci-i18n-ttyd-zh-cn=y
-CONFIG_PACKAGE_luci-i18n-watchcat-zh-cn=y
+#CONFIG_PACKAGE_luci-app-watchcat=y
+#CONFIG_PACKAGE_luci-app-ttyd=y
+#CONFIG_PACKAGE_luci-i18n-ttyd-zh-cn=y
+#CONFIG_PACKAGE_luci-i18n-watchcat-zh-cn=y
 EOF
 # 关闭 IPv6 节省空间
 # CONFIG_IPV6=n
