c4eb1a1f77eec7a11b48fe8e62b3a3400b64435a
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-29 07:59:36 +0800

    Update diy24.sh

--- diy24.sh
+++ diy24.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-cd openwrt
-
-echo "Applying WR720N v3 DTS patch"
+echo "Adding WR720N v3 support"
 
 ############################
 # WR720N DTS
 ############################
-
+cd openwrt
 cat > target/linux/ath79/dts/ar9331_tplink_tl-wr720n.dtsi << 'EOF'
 #include "ar9331.dtsi"
 
@@ -15,181 +13,206 @@ cat > target/linux/ath79/dts/ar9331_tplink_tl-wr720n.dtsi << 'EOF'
 #include <dt-bindings/input/input.h>
 
 / {
-    aliases {
-        led-boot = &led_system;
-        led-failsafe = &led_system;
-        led-running = &led_system;
-        led-upgrade = &led_system;
-        label-mac-device = &eth0;
-    };
+	aliases {
+		led-boot = &led_system;
+		led-failsafe = &led_system;
+		led-running = &led_system;
+		led-upgrade = &led_system;
+		label-mac-device = &eth0;
+	};
 
-    keys {
-        compatible = "gpio-keys";
+	keys {
+		compatible = "gpio-keys";
 
-        reset {
-            label = "reset";
-            linux,code = <KEY_RESTART>;
-            gpios = <&gpio 11 GPIO_ACTIVE_LOW>;
-            debounce-interval = <60>;
-        };
+		reset {
+			label = "reset";
+			linux,code = <KEY_RESTART>;
+			gpios = <&gpio 11 GPIO_ACTIVE_LOW>;
+			debounce-interval = <60>;
+		};
+		
+		sw1 {
+			label = "sw1";
+			linux,code = <BTN_0>;
+			gpios = <&gpio 18 GPIO_ACTIVE_LOW>;
+			debounce-interval = <60>;
+		};
+		
+		sw2 {
+			label = "sw2";
+			linux,code = <BTN_1>;
+			gpios = <&gpio 20 GPIO_ACTIVE_LOW>;
+			debounce-interval = <60>;
+		};
+	};
 
-        sw1 {
-            label = "sw1";
-            linux,input-type = <EV_SW>;
-            linux,code = <BTN_0>;
-            gpios = <&gpio 18 GPIO_ACTIVE_LOW>;
-            debounce-interval = <60>;
-        };
+	leds {
+		compatible = "gpio-leds";
 
-        sw2 {
-            label = "sw2";
-            linux,input-type = <EV_SW>;
-            linux,code = <BTN_1>;
-            gpios = <&gpio 20 GPIO_ACTIVE_LOW>;
-            debounce-interval = <60>;
-        };
-    };
+		led_system: system {
+			label = "blue:system";
+			gpios = <&gpio 27 GPIO_ACTIVE_LOW>;
+		};
+	};
+	
+	reg_usb_vbus: regulator {
+		compatible = "regulator-fixed";
+		regulator-name = "usb_vbus";
+		regulator-min-microvolt = <5000000>;
+		regulator-max-microvolt = <5000000>;
+		gpios = <&gpio 8 GPIO_ACTIVE_HIGH>;
+		enable-active-high;
+		regulator-boot-on;
+        regulator-always-on;
+	};
+ 
+};
 
-    leds {
-        compatible = "gpio-leds";
+&spi {
+	status = "okay";
 
-        led_system: system {
-            label = "blue:system";
-            gpios = <&gpio 27 GPIO_ACTIVE_LOW>;
-        };
-    };
+	flash@0 {
+		compatible = "jedec,spi-nor";
+		reg = <0>;
+		spi-max-frequency = <25000000>;
 
-    reg_usb_vbus: regulator {
-        compatible = "regulator-fixed";
-        regulator-name = "usb_vbus";
-        regulator-min-microvolt = <5000000>;
-        regulator-max-microvolt = <5000000>;
-        gpio = <&gpio 8 GPIO_ACTIVE_HIGH>;
-        enable-active-high;
-    };
+		partitions {
+			compatible = "fixed-partitions";
+			#address-cells = <1>;
+			#size-cells = <1>;
+
+			uboot: partition@0 {
+				reg = <0x0 0x20000>;
+				label = "u-boot";
+				read-only;
+			};
+
+			partition@20000 {
+				compatible = "tplink,firmware";
+				reg = <0x20000 0xfd0000>;
+				label = "firmware";
+			};
+
+			art: partition@ff0000 {
+				reg = <0xff0000 0x10000>;
+				label = "art";
+				read-only;
+			};
+		};
+	};
 };
 
 &eth0 {
-    status = "okay";
-    nvmem-cells = <&mac_eth0>;
+	status = "okay";
+	phy-mode = "mii";
+
+	nvmem-cells = <&macaddr_uboot_mac0>;
     nvmem-cell-names = "mac-address";
 
-    gmac-config {
-        device = <&gmac>;
-        switch-phy-addr-swap = <0>;
-        switch-phy-swap = <0>;
-    };
+	/* ===== 21.02 启动修复关键 ===== */
+	force-link = <1>;
+
+	fixed-link {
+		speed = <100>;
+		full-duplex;
+	};
+
+	gmac-config {
+		device = <&gmac>;
+		switch-phy-addr-swap = <0>;
+		switch-phy-swap = <0>;
+	};
 };
 
 &eth1 {
     status = "okay";
-    nvmem-cells = <&mac_eth1>;
+    nvmem-cells = <&macaddr_uboot_mac0>;
     nvmem-cell-names = "mac-address";
+    mac-address-increment = <1>; /* LAN = WAN + 1 */
 };
 
-&wmac {
-    status = "okay";
-    nvmem-cells = <&mac_wifi>;
-    nvmem-cell-names = "mac-address";
-};
+&builtin_switch {
+	status = "okay";
+	enable-vlan;
 
+	reset-gpios = <&gpio 12 GPIO_ACTIVE_LOW>;
+};
 &usb {
-    status = "okay";
-    dr_mode = "host";
-    vbus-supply = <&reg_usb_vbus>;
+	compatible = "generic-ehci";
+    has-transaction-translator;
+    caps-offset = <0x100>;
+	dr_mode = "host";
+	vbus-supply = <&reg_usb_vbus>;
+	status = "okay";
 };
 
 &usb_phy {
-    status = "okay";
+	status = "okay";
 };
+
+&wmac {
+	status = "okay";
+
+	mtd-cal-data = <&art 0x1000>;
+	nvmem-cells = <&macaddr_art_wifi>;
+    nvmem-cell-names = "mac-address";
+
+};
+/* U-Boot MAC provider */
 &uboot {
     compatible = "nvmem-cells";
 
-    mac_eth0: macaddr@124e0 {
-        reg = <0x124e0 0x6>;
+    macaddr_uboot_mac0: macaddr@124e0 {
+        reg = <0x124e0 0x6>; /* WAN MAC */
     };
 
-    mac_eth1: macaddr@124e6 {
-        reg = <0x124e6 0x6>;
-    };
-
-    mac_wifi: macaddr@1fc00 {
-        reg = <0x1fc00 0x6>;
+    macaddr_art_wifi: macaddr@1fc00 {
+        reg = <0x1fc00 0x6>; /* Wi-Fi MAC */
     };
 };
-&spi {
-    status = "okay";
+&mdio0 {
+	status = "okay";
 
-    flash@0 {
-        compatible = "jedec,spi-nor";
-        reg = <0>;
-        spi-max-frequency = <25000000>;
-
-        partitions {
-            compatible = "fixed-partitions";
-            #address-cells = <1>;
-            #size-cells = <1>;
-
-            uboot: partition@0 {
-                reg = <0x0 0x20000>;
-                label = "u-boot";
-                read-only;
-            };
-
-            partition@20000 {
-                compatible = "tplink,firmware";
-                reg = <0x20000 0xfd0000>;
-                label = "firmware";
-            };
-
-            art: partition@ff0000 {
-                reg = <0xff0000 0x10000>;
-                label = "art";
-                read-only;
-            };
-        };
-    };
+	phy0: ethernet-phy@0 {
+		reg = <0>;
+	};
 };
 EOF
 
-############################
-# WR720N v3 Device DTS
-############################
 
 cat > target/linux/ath79/dts/ar9331_tplink_tl-wr720n-v3.dts << 'EOF'
 #include "ar9331_tplink_tl-wr720n.dtsi"
 
 / {
-    model = "TP-Link TL-WR720N v3";
-    compatible = "tplink,tl-wr720n-v3", "tplink,tl-wr720n", "qca,ar9331";
+	model = "TP-Link TL-WR720N v3";
+	compatible = "tplink,tl-wr720n-v3", "tplink,tl-wr720n", "qca,ar9331";
 };
 EOF
 
 ############################
 # Device profile
 ############################
+sed -i '/tplink_tl-wr720n-v3/,+8d' target/linux/ath79/image/generic-tp-link.mk
 
 cat >> target/linux/ath79/image/generic-tp-link.mk << 'EOF'
-define Device/tplink_tl-wr720n-v3
-  $(Device/tplink-16mlzma)
+define Device/tplink_tl-wr720n-v3 
+  $(Device/tplink-16mlzma) 
   SOC := ar9331
   DEVICE_MODEL := TL-WR720N
   DEVICE_VARIANT := v3
+  DEVICE_PACKAGES := kmod-usb-core kmod-usblp kmod-usb2
   TPLINK_HWID := 0x07200103
-  DEVICE_PACKAGES := kmod-usb-core kmod-usb2
-  SUPPORTED_DEVICES := tl-wr720n-v3
+  SUPPORTED_DEVICES += tl-wr720n
 endef
 TARGET_DEVICES += tplink_tl-wr720n-v3
 EOF
 
 ############################
-# Build config
+# Fix USB (very important)
 ############################
+sed -i 's/"chipidea,usb2"/"generic-ehci"/g' target/linux/ath79/dts/ar9330.dtsi
 
-cat > .config << 'EOF'
-CONFIG_TARGET_ath79=y
-CONFIG_TARGET_ath79_generic=y
-CONFIG_TARGET_ath79_generic_DEVICE_tplink_tl-wr720n-v3=y
-EOF
+sed -i '/usb@1b000000 {/a\        has-transaction-translator;\n        caps-offset = <0x100>;' target/linux/ath79/dts/ar9330.dtsi
+sed -i '/usb@1b000000 {/,/};/ s/status = "disabled"/status = "okay"/' target/linux/ath79/dts/ar9330.dtsi
 
-echo "WR720N v3 DTS patch applied successfully"
+echo "WR720N patch applied"
