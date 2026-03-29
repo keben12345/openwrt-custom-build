ff9115fb9614f89d7e3bfd3463a8b11dcc06cef5
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-03-30 07:31:48 +0800

    Update diy25.sh

--- diy25.sh
+++ diy25.sh
@@ -16,6 +16,13 @@ target/linux/ath79/dts/ar9331_tplink_tl-wr710n.dtsi
 # 6. USB 供电
 sed -i '/regulator-name = "usb_vbus";/a\        gpios = <\&gpio 8 GPIO_ACTIVE_HIGH>;\n        regulator-always-on;' \
 target/linux/ath79/dts/ar9331_tplink_tl-wr710n.dtsi
+############################
+# Fix USB (very important)
+############################
+sed -i 's/"chipidea,usb2"/"generic-ehci"/g' target/linux/ath79/dts/ar9330.dtsi
+
+sed -i '/usb@1b000000 {/a\        has-transaction-translator;\n        caps-offset = <0x100>;' target/linux/ath79/dts/ar9330.dtsi
+sed -i '/usb@1b000000 {/,/};/ s/status = "disabled"/status = "okay"/' target/linux/ath79/dts/ar9330.dtsi
 
 # 7. 改设备名为 WR720N v3
 sed -i 's/TL-WR710N/TL-WR720N v3/g' \
